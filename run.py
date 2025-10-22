3d79749e8ec03f13050c0738b49cd35612e63f64
Eugene Dvorcoviy <76780101+EvgeniyZharov@users.noreply.github.com>
2025-10-22 11:02:33 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ app = create_app()
 # app.config['PROPAGATE_EXCEPTIONS'] = True
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8020, debug=True)
+    # app.run(host='0.0.0.0', port=8020, debug=True)
     # app.run(host='0.0.0.0', port=8020, debug=False)
-    # app.run(port=80, debug=True)
+    app.run(port=80, debug=True)
+
